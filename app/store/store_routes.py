46d7a5e334cc62869fcaee1a7226db3d0ed7e5ba
jhilikkundu <jhilikkundu@localhost.localdomain>
2020-05-28 17:59:36 +0530

    storeitem committed

--- app/store/store_routes.py
+++ app/store/store_routes.py
@@ -5,7 +5,14 @@ class Item(Resource):
     def get(self,ItemID):
         
 
-    def post():
+   def post(self,ItemId):
+        ItemN=request.form["Name"]
+        email=request.form["Email"]
+        account_type=request.form["account_type"]
+        err=add_user.addUser(mobile_num,name,email,account_type)
+        if err != True:
+            return response.styler(204)
+        return response.styler(400)
 
     def put():
 
